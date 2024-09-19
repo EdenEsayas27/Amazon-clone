33f21c773249975449d23034580f7dcbb1753a75
Eden Esayas Tesema <edenesayas1627@gmail.com>
2024-09-20 02:54:22 +0300

    carousel added

---
+++ Amazon-clone/src/Components/Carousel/CarouselEffect.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
+import { img } from "./img/data";
+import classes from "./carousel.module.css";
+function CarouselEffect() {
+  return (
+    <div>
+      <Carousel
+        autoPlay={true} // Ensure correct casing
+        infiniteLoop={true}
+        showIndicators={false}
+        showThumbs={false}
+        interval={3000} // Set the interval in milliseconds
+        stopOnHover={true}
+      >
+        {img.map((imageLink) => {
+          return <img src={imageLink} />;
+        })}
+      </Carousel>
+       <div className={classes.hero__img}></div> 
+    </div>
+  );
+ 
+}
+ export default CarouselEffect;
