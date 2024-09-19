5b85abce48589c9e0a2fdad9721e0f267000d9fc
Eden Esayas Tesema <edenesayas1627@gmail.com>
2024-09-20 01:09:04 +0300

    lower header and responsive

--- Amazon-clone/src/Components/Header/Header.jsx
+++ Amazon-clone/src/Components/Header/Header.jsx
@@ -3,62 +3,66 @@ import { SlLocationPin } from "react-icons/sl";
 import { BsSearch } from "react-icons/bs";
 import { BiCart } from "react-icons/bi";
 import classes from "./header.module.css"
+import LowerHeader from "./LowerHeader";
 
 function Header() {
   return (
-    <section>
+    <>
       <section>
-        <div className={classes.header__container}>
-          <div className={classes.logo__container}>
-            <a href="#">
-              <img
-                src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
-                alt="amazon logo"
-              />
-            </a>
-            <div className={classes.delivery}>
-              <span>
-                <SlLocationPin />
-              </span>
-              <div>
-                <p>Delivered to </p>
-                <span>Ethiopia</span>
+        <section>
+          <div className={classes.header__container}>
+            <div className={classes.logo__container}>
+              <a href="#">
+                <img
+                  src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
+                  alt="amazon logo"
+                />
+              </a>
+              <div className={classes.delivery}>
+                <span>
+                  <SlLocationPin />
+                </span>
+                <div>
+                  <p>Delivered to </p>
+                  <span>Ethiopia</span>
+                </div>
               </div>
             </div>
-          </div>
-          <div className={classes.search}>
-            <select name="" id="">
-              <option value="">All</option>
-            </select>
-            <input type="text" />
-            <BsSearch size={25} />
-          </div>
-          <div className={classes.order__container}>
-            <a href="" className={classes.language}>
-              <img
-                src="https://pngimg.com/uploads/flags/flags_PNG14592.png"
-                alt="flag"
-              />
+            <div className={classes.search}>
               <select name="" id="">
-                <option value="">EN</option>
+                <option value="">All</option>
               </select>
-            </a>
-            <a href="">
-              <p>Sign In</p>
-              <span>Account & Lists</span>
-            </a>
-            <a href="">
-              <p>returns</p>
-              <span>&orders</span>
-            </a>
-            <a href="" className={classes.cart}>
-              <BiCart size={35} />
-              <span>0</span>
-            </a>
+              <input type="text" />
+              <BsSearch size={25} />
+            </div>
+            <div className={classes.order__container}>
+              <a href="" className={classes.language}>
+                <img
+                  src="https://pngimg.com/uploads/flags/flags_PNG14592.png"
+                  alt="flag"
+                />
+                <select name="" id="">
+                  <option value="">EN</option>
+                </select>
+              </a>
+              <a href="">
+                <p>Sign In</p>
+                <span>Account & Lists</span>
+              </a>
+              <a href="">
+                <p>returns</p>
+                <span>&orders</span>
+              </a>
+              <a href="" className={classes.cart}>
+                <BiCart size={35} />
+                <span>0</span>
+              </a>
+            </div>
           </div>
-        </div>
+        </section>
       </section>
-    </section>
+      <LowerHeader />
+    </>
   );
 }
 
