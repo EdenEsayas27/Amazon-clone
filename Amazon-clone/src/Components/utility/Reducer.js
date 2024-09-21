9938cead964045061dfa6c3590edd2d88ea4919c
Eden Esayas Tesema <edenesayas1627@gmail.com>
2024-09-21 07:17:19 +0300

    cart page price of single product multiplied by its amount

--- Amazon-clone/src/Components/utility/Reducer.js
+++ Amazon-clone/src/Components/utility/Reducer.js
@@ -1,17 +1,38 @@
+import { Type } from "./action.type";
 
-import {Type} from './action.type'
 export const initialState = {
-    basket: [],
+  basket: [],
 };
 
 export const reducer = (state, action) => {
   switch (action.type) {
     case Type.ADD_TO_BASKET:
-      return {
-        ...state,
-        basket: [...state.basket, action.item],
-      };
+      // Check if the item already exists in the basket
+      const existingItem = state.basket.find(
+        (item) => item.id === action.item.id
+      );
+
+      if (!existingItem) {
+        // Item does not exist, add it to the basket
+        return {
+          ...state,
+          basket: [...state.basket, { ...action.item, amount: 1 }],
+        };
+      } else {
+        // Item exists, update the amount
+        const updatedBasket = state.basket.map((item) => {
+          return item.id === action.item.id
+            ? { ...item, amount: item.amount + 1 }
+            : item;
+        });
+
+        return {
+          ...state,
+          basket: updatedBasket,
+        };
+      }
+
     default:
-      return state;
+      return state; // Return the current state for unhandled actions
   }
 };
