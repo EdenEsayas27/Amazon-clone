50eea5e16fc7b708e7883c2c2a51e57a96f6da1c
Eden Esayas Tesema <edenesayas1627@gmail.com>
2024-09-27 04:27:46 +0300

    payment page UI added

--- Amazon-clone/src/Router.jsx
+++ Amazon-clone/src/Router.jsx
@@ -7,6 +7,11 @@ import Order from './pages/order/Order'
 import Cart from './pages/cart/Cart'
 import Results from './pages/Results/Results'
 import ProductDetail from './pages/productDetail/ProductDetail'
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+const stripePromise = loadStripe(
+  "pk_test_51Q2iEqKtL52B7yJxpjVhmgnYJOj3fcUMU8LToXBxn1J8Jdk68f59BJRqtDCdCwGOShhYqATZxufMwA8u4qtTvNSP00yeQGSM3P"
+);
 
 
 function Routing() {
@@ -15,11 +20,18 @@ function Routing() {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/auth" element={<Auth />} />
-        <Route path="/payments" element={<Payment />} />
+        <Route
+          path="/payments"
+          element={
+            <Elements stripe={stripePromise}>
+              <Payment />
+            </Elements>
+          }
+        />
         <Route path="/orders" element={<Order />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/category/:categoryName" element={<Results />} />
-        <Route path="/products/:productId"    element={<ProductDetail />} />
+        <Route path="/products/:productId" element={<ProductDetail />} />
       </Routes>
     </Router>
   );
--- Amazon-clone/src/pages/payment/Payment.jsx
+++ Amazon-clone/src/pages/payment/Payment.jsx
@@ -1,9 +1,73 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import Layout from '../../Components/Layout/Layout'
+import classes from './payment.module.css'
+import { DataContext } from '../../Components/DataProvider/DataProvider'
+import ProductCard from '../../Components/Product/ProductCard'
+import {useStripe, useElements, CardElement} from '@stripe/react-stripe-js';
+import CurrencyFormater from '../../Components/CurrencyFormater/CurrencyFormater'
 function Payment() {
+   const[{user,basket}]=useContext(DataContext);
+    const totalItem = basket?.reduce((amount, item) => {
+      return item.amount + amount;
+    }, 0);
+      const total = basket.reduce((amount, item) => {
+        return item.price * item.amount + amount;
+      }, 0);
+    const[cardError,setCardError] = useState(null)
+    const stripe = useStripe();
+    const elements = useElements();
+   const handelChange= (e)=>{
+      console.log(e);
+      e?.error?.message? setCardError(e?.error?.message):setCardError("")
+    }
   return (
     <Layout>
-      payment
+      {/* header */}
+      <div className={classes.payment__header}>checkout ({totalItem}) items</div>
+      {/* payment method */}
+      <section className={classes.payment}>
+        {/* address */}
+        <div className={classes.flex}>
+          <h3>Delivery Address</h3>
+        </div>
+        <div>{user?.email}</div>
+        <div>123 React Lane</div>
+        <div>ETH,AA</div>
+        <hr />
+        {/* product */}
+        <div className={classes.flex}>
+          <h3>Review items and delivery</h3>
+          <div>
+            {
+              basket?.map((item)=><ProductCard product={item} flex={true}/>)
+            }
+          </div>
+        </div>
+        <hr />
+        {/* card from */}
+        <div className={classes.flex}> 
+          <h3>payment methods</h3>
+          <div className={classes.payment__card__container}>
+            <div className={classes.payment__details}>
+              <form action="">
+                  {/* error */}
+                   {cardError && <small style={{color:"red"}}>{cardError}</small>}
+                   {/* card element */}
+                    <CardElement onChange={handelChange}/>
+                    {/* price */}
+                    <div className={classes.payment__price}>
+                      <div>
+                        <span style={{display:"flex", gap:"10px"}} >
+                          Total Order | <CurrencyFormater amount={total} />
+                        </span>
+                      </div>
+                      <button>Pay Now</button>
+                    </div>
+                  </form>
+            </div>
+          </div>
+        </div>
+      </section>
     </Layout>
   )
 }
