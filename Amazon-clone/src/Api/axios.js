7d0962589c3be1c03846ca09dca79765a334b305
Eden Esayas Tesema <edenesayas1627@gmail.com>
2024-09-28 05:31:16 +0300

    axios baseURL replaced with deployed version ofamazon server on render.com

--- Amazon-clone/src/Api/axios.js
+++ Amazon-clone/src/Api/axios.js
@@ -1,5 +1,9 @@
 import axios from "axios"
 const axiosInstance = axios.create({
-    baseURL: "http://127.0.0.1:3000"
+    //localversion
+   // baseURL: "http://127.0.0.1:3000"
+   //deployed version  of amazon server on render.com
+   baseURL:"https://amazon-api-deploy-1-dfxs.onrender.com/"
 })
 export {axiosInstance};
+git rebase -i HEAD~n
